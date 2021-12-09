c35c9c25dd45d06acf1f70511ff80d18221ebcc0
ajjankowski <aj.jankowski92@gmail.com>
2021-12-09 13:56:11 +0100

    Add ResultsPage Refactor HotelSearchTest

--- test/pl/seleniumdemo/pages/HotelSearchPage.java
+++ test/pl/seleniumdemo/pages/HotelSearchPage.java
@@ -1,5 +1,6 @@
 package pl.seleniumdemo.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,9 +13,6 @@ public class HotelSearchPage {
 
     @FindBy(xpath = "//div[@id='select2-drop']//input")
     private WebElement searchHotelInput;
-
-    @FindBy(xpath = "//span[@class='select2-match' and text()='Dubai']")
-    private WebElement hotelMarch;
 
     @FindBy(name = "checkin")
     private WebElement checkinInput;
@@ -34,14 +32,18 @@ public class HotelSearchPage {
     @FindBy(xpath = "//button[text()=' Search']")
     private WebElement searchButton;
 
+    private WebDriver driver;
+
     public HotelSearchPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
+        this.driver = driver;
     }
 
     public void setCity(String cityName) {
         searchHotelSpan.click();
         searchHotelInput.sendKeys(cityName);
-        hotelMarch.click();
+        String xpath = String.format("//span[@class='select2-match' and text()='%s']", cityName);
+        driver.findElement(By.xpath(xpath)).click();
     }
 
     public void setDates(String checkin, String checkout) {
@@ -49,10 +51,16 @@ public class HotelSearchPage {
         checkoutInput.sendKeys(checkout);
     }
 
-    public void setTravellers() {
+    public void setTravellers(int adultsToAdd, int childToAdd) {
         travellersInput.click();
-        adultPlusBtn.click();
-        childPlusBtn.click();
+        addTraveler(adultPlusBtn, adultsToAdd);
+        addTraveler(childPlusBtn, childToAdd);
+    }
+
+    private void addTraveler(WebElement travelerBtn, int numberOfTravelers) {
+        for (int i=0; i< numberOfTravelers; i++) {
+            travelerBtn.click();
+        }
     }
 
     public void performSearch() {
--- test/pl/seleniumdemo/tests/HotelSearchTest.java
+++ test/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -1,31 +1,24 @@
 package pl.seleniumdemo.tests;
 
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import pl.seleniumdemo.pages.HotelSearchPage;
-
+import pl.seleniumdemo.pages.ResultsPage;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HotelSearchTest extends BaseTest {
 
     @Test
     public void searchHotelTest() {
         setup();
-
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
         hotelSearchPage.setCity("Dubai");
         hotelSearchPage.setDates("17/04/2025", "20/04/2025");
-        hotelSearchPage.setTravellers();
+        hotelSearchPage.setTravellers(1, 2);
         hotelSearchPage.performSearch();
 
-        List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class,'list_title')]//b"))
-                .stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
-
+        ResultsPage resultsPage = new ResultsPage(driver);
+        List<String> hotelNames = resultsPage.getHotelNames();
         System.out.println(hotelNames.size());
         hotelNames.forEach(System.out::println);
 
@@ -39,14 +32,15 @@ public class HotelSearchTest extends BaseTest {
     @Test
     public void searchHotelWithoutNameTest() {
         setup();
-        driver.findElement(By.name("checkin")).sendKeys("17/04/2025");
-        driver.findElement(By.name("checkout")).sendKeys("20/04/2025");
-        driver.findElement(By.id("travellersInput")).click();
-        driver.findElement(By.id("childPlusBtn")).click();
-        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setDates("17/04/2025", "20/04/2025");
+        hotelSearchPage.setTravellers(0, 1);
+        hotelSearchPage.performSearch();
 
-        String noResultHeading = driver.findElement(By.xpath("//div[@class='itemscontainer']")).getText();
-        Assert.assertEquals(noResultHeading, "No Results Found");
+        ResultsPage resultsPage= new ResultsPage(driver);
+
+        Assert.assertTrue(resultsPage.resultHeading.isDisplayed());
+        Assert.assertEquals(resultsPage.getHeadingText(), "No Results Found");
         tearDown();
     }
 
