9d5beca56c00fdbf8be833f4c99e8431414da0bd
ajjankowski <aj.jankowski92@gmail.com>
2022-01-31 18:01:26 +0100

    Last refactor

--- src/test/test/pl/selenium/utils/SeleniumHelper.java
+++ src/test/test/pl/selenium/utils/SeleniumHelper.java
@@ -13,17 +13,17 @@ import java.io.IOException;
 public class SeleniumHelper {
 
     public static void waitForElementToExist(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, 10l);
+        WebDriverWait wait = new WebDriverWait(driver, 10L);
         wait.until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 
     public static void waitForElementToBeVisible(WebDriver driver, WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, 10l);
+        WebDriverWait wait = new WebDriverWait(driver, 10L);
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public static void waitForNotEmptyList(WebDriver driver, By locator) {
-        WebDriverWait wait = new WebDriverWait(driver, 10l);
+        WebDriverWait wait = new WebDriverWait(driver, 10L);
         wait.until(browser -> browser.findElements(locator).size() > 0);
     }
 
